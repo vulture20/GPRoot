12f012540320e5c8495a0d5e4958c7061ed07f01
vulture20 <vulture20@web.de>
2015-02-13 10:11:49 +0100

    First commit

---
+++ GPRoot/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GPRoot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Installation-Kennwort:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Root-Passwort:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 101);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GPRoot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ GPRoot/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GPRoot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string convertPassword(string pass)
+        {
+            string[,] lookupTable =
+            {
+                {"a", "b"}, {"b", "c"}, {"c", "e"}, {"d", "g"}, {"e", "h"}, {"f", "n"}, {"g", "i"}, {"h", "j"}, {"i", "d"}, {"j", "k"}, {"k", "a"}, {"l", "l"}, {"m", "m"},
+                {"n", "p"}, {"o", "r"}, {"p", "o"}, {"q", "f"}, {"r", "s"}, {"s", "t"}, {"t", "u"}, {"u", "q"}, {"v", "w"}, {"w", "x"}, {"x", "y"}, {"y", "z"}, {"z", "v"},
+                {"A", "B"}, {"B", "C"}, {"C", "E"}, {"D", "G"}, {"E", "H"}, {"F", "N"}, {"G", "I"}, {"H", "J"}, {"I", "D"}, {"J", "K"}, {"K", "A"}, {"L", "L"}, {"M", "M"},
+                {"N", "P"}, {"O", "R"}, {"P", "O"}, {"Q", "F"}, {"R", "S"}, {"S", "T"}, {"T", "U"}, {"U", "Q"}, {"V", "W"}, {"W", "X"}, {"X", "Y"}, {"Y", "Z"}, {"Z", "V"},
+                {"0", "1"}, {"1", "9"}, {"2", "2"}, {"3", "6"}, {"4", "3"}, {"5", "4"}, {"6", "7"}, {"7", "5"}, {"8", "0"}, {"9", "8"}
+            };
+
+            string temp = pass;
+            int i, j;
+
+            for (i = 0; i < pass.Length; i++)
+            {
+                for (j = 0; j < lookupTable.GetLength(0); j++)
+                {
+                    if (pass[i].ToString().Equals(lookupTable[j, 0]))
+                    {
+//                        temp[i] = lookupTable[j, 1];
+//                        temp = temp.Substring(0, i - 1) + lookupTable[j, 1] + temp.Substring(i + 1, temp.Length - i + 2);
+                        temp = temp.Remove(i, 1).Insert(i, lookupTable[j, 1]);
+                        break;
+                    }
+                }
+            }
+
+            return temp;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = convertPassword(textBox1.Text);
+        }
+    }
+}
